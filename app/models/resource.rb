6f250f9ae340c377ab7e3895d85f97fcbeeeb948
James Gates <gatorjuice@gmail.com>
2018-12-18 21:08:06 -0600

    added geocoder

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,11 @@
 class Resource < ApplicationRecord
+  enum status: [:unverified, :verified]
+
+  geocoded_by :address
+
+  after_validation :geocode
+
+  def address
+    [street, city, state, zipcode].join(', ')
+  end
 end

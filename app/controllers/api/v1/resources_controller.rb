cf69e9a005264dc0261b444f8ba9a509a5d2f856
James Gates <gatorjuice@gmail.com>
2018-12-18 20:29:30 -0600

    declared controller actions

--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::ResourcesController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end

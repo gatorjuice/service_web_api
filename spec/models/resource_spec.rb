1918685cd09734e554339f07df668a8737a13b1a
James Gates <gatorjuice@gmail.com>
2018-12-19 00:46:39 -0600

    added factory and got that working with some stubs

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
+Geocoder::Lookup::Test.add_stub(
+  '2400 N Western Ave, Chicago, IL, 60625', [
+    { 'coordinates' => [41.90154, -87.694389] }
+  ]
+)
+
 RSpec.describe Resource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.food' do
+    let(:food_resource) { create(:resource, :food) }
+
+    before do
+      create(:resource, :health)
+      create(:resource, :shelter)
+    end
+
+    it 'returns resources with food' do
+      expect(Resource.food).to match_array([food_resource])
+    end
+  end
+
+  describe '.health' do
+    let(:health_resource) { create(:resource, :health) }
+
+    before do
+      create(:resource, :food)
+      create(:resource, :shelter)
+    end
+
+    it 'returns resources with health' do
+      expect(Resource.health).to match_array([health_resource])
+    end
+  end
+
+  describe '.shelter' do
+    let(:shelter_resource) { create(:resource, :shelter) }
+
+    before do
+      create(:resource, :food)
+      create(:resource, :health)
+    end
+
+    it 'returns resources with shelter' do
+      expect(Resource.shelter).to match_array([shelter_resource])
+    end
+  end
 end

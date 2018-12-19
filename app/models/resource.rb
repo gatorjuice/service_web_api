10e2ae08ef59c1919f93ba760e9d6904378e0845
James Gates <gatorjuice@gmail.com>
2018-12-18 22:52:56 -0600

    setup close and closest self methods

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,22 @@
 class Resource < ApplicationRecord
+  RADIUS = 10 # miles
+
   enum status: [:unverified, :verified]
 
   after_validation :geocode
 
   geocoded_by :address
+
+  scope :food, -> () { where(food: true) }
+  scope :health, -> () { where(health: true) }
+  scope :shelter, -> () { where(shelter: true) }
+
+  scope :close, -> (lat, long) { near([lat, long], RADIUS) }
+  scope :closest, -> (lat, long) do
+    min_by { |resource| resource.distance_from([lat, long]) }
+  end
+
+  private
 
   def address
     [street, city, state, zipcode].join(', ')

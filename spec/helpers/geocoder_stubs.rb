bc356d2e5dc9d008b3cb1294226f348ee431e266
James Gates <gatorjuice@gmail.com>
2018-12-25 19:06:56 -0600

    corrected some things in the spec folder

--- spec/helpers/geocoder_stubs.rb
+++ spec/helpers/geocoder_stubs.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Geocoder::Lookup::Test.set_default_stub(
   [
     {
-      'coordinates'  => [41.90154, -87.694389],
-      'address'      => 'Chicago, IL, USA',
-      'state'        => 'Illinois',
-      'state_code'   => 'IL',
-      'country'      => 'United States',
+      'coordinates' => [41.90154, -87.694389],
+      'address' => 'Chicago, IL, USA',
+      'state' => 'Illinois',
+      'state_code' => 'IL',
+      'country' => 'United States',
       'country_code' => 'US'
     }
   ]
@@ -14,11 +16,11 @@ Geocoder::Lookup::Test.set_default_stub(
 Geocoder::Lookup::Test.add_stub(
   '2400 N Western Ave, Chicago, IL, 60625', [
     {
-      'coordinates'  => [41.9257047, -87.6896371],
-      'address'      => 'Chicago, IL, USA',
-      'state'        => 'Illinois',
-      'state_code'   => 'IL',
-      'country'      => 'United States',
+      'coordinates' => [41.9257047, -87.6896371],
+      'address' => 'Chicago, IL, USA',
+      'state' => 'Illinois',
+      'state_code' => 'IL',
+      'country' => 'United States',
       'country_code' => 'US'
     }
   ]
